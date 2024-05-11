0a5b535aca3b164a4925d0a6ef845662bf87306b
muhammedahmetsekerci <96115180+muhammedahmetsekerci@users.noreply.github.com>
2024-05-11 14:14:49 +0300

    Episode 1 player controls and recipe added.

---
+++ Assets/1.bolum/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rgb;
+    Vector3 velocity;
+
+    float speedAmount = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rgb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity = new Vector3(Input.GetAxis("Horizontal"), 0f);
+        transform.position += velocity * speedAmount * Time.deltaTime;
+
+    }
+}
